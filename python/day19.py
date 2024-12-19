048ab9fe932ae895908cc7c95160ca8720f93191
Borin Ouch <borin.ouch@gmail.com>
2024-12-19 01:15:12 -0800

    Solve day19 part 2

--- python/day19.py
+++ python/day19.py
@@ -48,8 +48,6 @@ def minimize_towel_set(towels):
 
 
 def possible_designs(designs, towels):
-    towels = minimize_towel_set(towels)
-
     possible = []
     for design in designs:
         if is_possible_design(design, towels):
@@ -57,14 +55,48 @@ def possible_designs(designs, towels):
 
     return possible
 
+
+def count_arrangements(design, towels):
+    if not is_possible_design(design, minimize_towel_set(towels)):
+        return 0
+
+    min_len = min([len(t) for t in towels])
+    max_len = max([len(t) for t in towels])
+    memo = {}
+
+    def count(design):
+        if design == "":
+            return 1
+
+        if design in memo:
+            return memo[design]
+
+        res = 0
+        i = 0
+        for n in range(min(len(design), min_len), min(len(design) + 1, max_len + 1)):
+            i += 1
+            head = design[:n]
+            if head in towels:
+                res += count(design[n:])
+
+        memo[design] = res
+        return res
+
+    res = count(design)
+    return res
+
+
 def part1(lines):
     towels, designs = parse_towels(lines)
+    towels = minimize_towel_set(towels)
     possible = possible_designs(designs, towels)
     return len(possible)
 
 
 def part2(lines):
-    pass
+    towels, designs = parse_towels(lines)
+    arrangements = [count_arrangements(design, towels) for design in designs]
+    return sum(arrangements)
 
 
 def main():
